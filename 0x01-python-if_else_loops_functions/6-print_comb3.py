3103eb82786395a5ea6db1db48f6cf4ca7f37876
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-03-13 13:44:32 -0700

    6.

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
 for first_digit in range(0, 10):
-    for second_digit in range(0, 10):
-        if first_digit != second_digit:
-            print((str(first_digit)), str(second_digit))
+    for second_digit in range(1, 10):
+        if first_digit > second_digit:
+            continue
+        else:
+            print(f"{first_digit}, {second_digit})
