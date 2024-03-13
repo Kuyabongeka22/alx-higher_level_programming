928e09fa64583f400989e1ea4e8597a5c0bfa004
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-03-13 12:35:38 -0700

    12.

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 
 def fizzbuzz():
-    for number in range(1,101):
-        if number % 3 == 0 | number % 5 == 0:
+    for number in range(1, 101):
+        if number % 3==0 | number % 5==0:
             print(f"FizzBuzz", end=' ')
-        elif number % 3 == 0:
+        elif number % 3==0:
             print(f"Fizz", end= ' ')
-        elif number % 5 == 0:
+        elif number % 5==0:
             print(f"Buzz", end=' ')
         else:
             print(f"{number}", end=' ')
