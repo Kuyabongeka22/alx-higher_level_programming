f07b0ce1bbeb6459741d2f86cec6b403823a837c
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-03-13 05:18:34 -0700

    8..

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -4,4 +4,4 @@ def uppercase(str):
     for letters in str:
         if ord(letters) > 96 and ord(letters) < 123:
             letters = chr(ord(letters) - 32)
-        print("{}".format(letters), end=' ')
+        print("{}".format(str))
