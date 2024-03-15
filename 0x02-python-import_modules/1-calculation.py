08027bc8279e4b493a3bfdc411ccb75882cd586d
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-03-15 01:40:23 -0700

    1.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,12 +7,9 @@ if __name__ == "__main__":
     b = 5
     addition = add(a, b)
     print("{} + {} = {}".format(a, b, addition))
-    
     difference = sub(a, b)
     print("{} - {} = {}".format(a, b, difference))
-    
     product = mul(a, b)
-    print("{} * {} = {}".format(a, b, product))
-    
+    print("{} * {} = {}".format(a, b, product)) 
     quotient = div(a, b)
     print("{} / {} = {}".format(a, b, quotient))
