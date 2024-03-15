5f43fc719f68d5b46254356100162b94720dfcb8
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-03-15 01:38:27 -0700

    1.

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,16 +3,16 @@
 if __name__ == "__main__":
     from calculator_1 import add, sub, mul, div
     
-    a= 10
-    b= 5
-    addition= add(a, b)
+    a = 10
+    b = 5
+    addition = add(a, b)
     print("{} + {} = {}".format(a, b, addition))
     
-    difference= sub(a, b)
+    difference = sub(a, b)
     print("{} - {} = {}".format(a, b, difference))
     
-    product= mul(a, b)
+    product = mul(a, b)
     print("{} * {} = {}".format(a, b, product))
     
-    quotient= div(a, b)
+    quotient = div(a, b)
     print("{} / {} = {}".format(a, b, quotient))
