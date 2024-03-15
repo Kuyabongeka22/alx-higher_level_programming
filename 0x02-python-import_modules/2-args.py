8f425ad651a975d82f0f679918f0f253a2a4155f
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-03-15 01:55:07 -0700

    2.

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,17 +1,16 @@
 #!/usr/bin/python3
-import sys
+if __name__ == "__main__":
+    import sys
 
-argv= sys.argv
-no_of_elements= len(argv) - 1
+    argv= sys.argv
+    no_of_elements= len(argv) - 1
 
-if no_of_elements == 0:
-    print("{} arguments.".format(no_of_elements))
-elif no_of_elements == 1:
-    print("{} argument:".format(no_of_elements))
-else:
-    print("{} arguments:".format(no_of_elements))
+    if no_of_elements == 0:
+        print("{} arguments.".format(no_of_elements))
+    elif no_of_elements == 1:
+        print("{} argument:".format(no_of_elements))
+    else:
+        print("{} arguments:".format(no_of_elements))
 
-total_arguments= len(argv)
-for i in range(1, total_arguments):
-    for number in range(1, total_arguments):
-        print("{}: {}".format(number, sys.argv[i]))
+    for number in range(no_of_elements):
+            print("{}: {}".format(number + 1, sys.argv[number + 1]))
