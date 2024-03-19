e5ecc489280f3c0a07e894f6cee6b634c9f95075
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-03-19 01:28:31 -0700

    1.

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    for i in my_list:   
-        element = my_list.index(i)
+    for element in my_list:   
+        element = my_list[idx]
     if idx < 0:
         return None
     elif idx > len(my_list):
