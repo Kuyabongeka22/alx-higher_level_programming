6dfbaa823534d18dc3aec191cb1bf7e9732ef647
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-04-03 04:54:34 -0700

    2.

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 
 def safe_print_list_integers(my_list=[], x=0):
-    for elements in range (x):
+    count = 0
+    for elements in range(x):
         try:
-            print("{:d}".format(my_list[elements]))
-        except (ValueError, IndexError):
-            pass
-        else:
-            return my_list[elements]
+            print("{:d}".format(my_list[elements]), end='')
+            count = count + 1
+        except (ValueError, TypeError):
+            continue
+    print("")
+    return count
