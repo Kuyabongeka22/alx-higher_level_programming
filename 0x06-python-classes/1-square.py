93bda986d24fafc8c3e98902eee5e13d362465fa
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-04-03 15:23:59 -0700

    1.

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""Define a class square"""
+
+
+class Square:
+    """A square class"""
+
+    def __init__(self, __size):
+        """defining private instance attribute size"""
+        self.__size = __size
