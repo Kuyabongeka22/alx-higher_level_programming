ae1b6ce7199c068591c59860e236d96a8493dce0
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-04-17 20:26:44 +0000

    6.

--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -7,5 +7,5 @@ import json
 
 def load_from_json_file(filename):
 	"""create from json"""
-	with open(filename) as file1:
-        return json.load(file1)
+	with open(filename, "r", encoding="UTF8") as file1:
+	        return json.load(file1)
