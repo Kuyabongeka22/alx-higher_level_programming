d5f138bb8cc45421c395d587000f3760632eabcd
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-04-17 18:13:29 +0000

    0.

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
+"""Defines a text file-reading function."""
+
 
 def read_file(filename=""):
+	"""Defines a text file-reading function."""
 	with open(filename, "r", encoding = "UTF8") as file:
 		print(file.read(), end="")
