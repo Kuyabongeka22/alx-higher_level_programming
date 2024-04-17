8ac3233adb5487482a2cfdddd0175a26844a14cd
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-04-17 18:23:48 +0000

    1.

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,7 +4,13 @@
 
 
 def write_file(filename="", text=""):
-	""" returns the number of characters written in a text file"""
+	""" returns the number of characters written in a text file
+	Args:
+	filename (str): The name of the file to write.
+	text (str): The text to write to the file.
+	Returns:
+	The number of characters written.
+	"""
 	with open(filename, mode="w", encoding = "UTF8") as file:
 		string = file.write(str())
 		return len(string)
