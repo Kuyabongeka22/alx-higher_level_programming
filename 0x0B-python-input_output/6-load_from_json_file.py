238a607955cd42236ffd0c5c70496ee38573c4f6
Kuyabongeka <mayezakuyabongeka@outlook.com>
2024-04-17 19:37:54 +0000

    6.

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""function that creates an Object from a JSON file"""
+import json
+
+
+def load_from_json_file(filename):
+	"""create from json"""
+	obj = json.loads(filename)
+	with open(filename, "w") as file:
+		file.write(obj)
